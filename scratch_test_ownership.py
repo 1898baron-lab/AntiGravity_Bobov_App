744622b63ad2a2a7ce90ef0873ea686fbe46869c
Sasha Baron <SashaBaron@example.com>
2026-05-19 09:18:20 +0500

    auto: synchronization update

---
+++ scratch_test_ownership.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+# Force utf-8 stdout
+sys.stdout.reconfigure(encoding='utf-8')
+
+from notebooklm_mcp.auth import load_cached_tokens
+from notebooklm_mcp.api_client import NotebookLMClient
+
+tokens = load_cached_tokens()
+os.environ['NOTEBOOKLM_AUTHUSER'] = '0'
+client = NotebookLMClient(cookies=tokens.cookies, csrf_token="", session_id="")
+
+nbs = client.list_notebooks()
+print('Authuser 0 count:', len(nbs))
+for nb in nbs:
+    print(f'- {nb.title} (Owned: {nb.is_owned}, Shared: {nb.is_shared})')
